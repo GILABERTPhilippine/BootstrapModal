809ab14e60185b3e8d4235c435ccf44d21a5fe72
PhilippineG <philippinegilabert@gmail.com>
2018-03-15 17:11:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $(".modal-title").html("Hôtel de la gare")
+    })
+    $("#btn2").click(function(){
+        $(".modal-title").html("Hôtel du port")
+    })
+    $("#btn3").click(function(){
+        $(".modal-title").html("Hôtel de l'angle de la rue")
+    })
+});
